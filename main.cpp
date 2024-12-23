ef0e1c28b767709bafe96dafda763e4ea1f5c86b
Frilyo <142653518+yoFrilyo@users.noreply.github.com>
2024-12-22 16:23:59 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,77 @@
 // main.cpp
 // By Frilyo and Augie!
 
-#include <nlohmann/json.hpp>
-#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include "tinyxml2.h"
 #include "SoundBite.h"
 
-const std::string SAVE_FILE_NAME = "SavedSounds.json";
+const std::string SAVE_FILE = "SavedSounds.xml";
+const std::string SAVE_FOLDER = "SoundFolder";
 
-nlohmann::json loadFile();
-bool saveFile(nlohmann::json SAVEFILE);
+void error(const std::string& FAILMESSAGE);
+tinyxml2::XMLElement* loadFile(const std::string& SAVEFILE, tinyxml2::XMLDocument& doc);
+bool saveFile(const std::string& SAVEFILE, tinyxml2::XMLDocument& doc);
 
 
 
 int main()
 {
-    nlohmann::json data = loadFile();
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLElement* root = loadFile(SAVE_FILE, doc);
 
 
 
-    saveFile(filepath);
+    saveFile(SAVE_FILE, doc);
 }
 
 
 
-nlohmann::json loadFile()
+tinyxml2::XMLElement* loadFile(const std::string& SAVEFILE, tinyxml2::XMLDocument& doc)
 {
-    std::fstream file(SAVE_FILE_NAME);
-    if (!file.is_open()){
-        throw std::runtime_error("Failed to open file: " + SAVE_FILE_NAME)
+    // find SAVEFILE
+    tinyxml2::XMLError eResult = doc.LoadFile(SAVEFILE.c_str());
+    if (eResult != tinyxml2::XML_SUCCESS){
+        std::cout << SAVEFILE << " not found.\nCreating file " << SAVEFILE << " ...\n";
+        
+        // create SAVEFILE
+        tinyxml2::XMLDeclaration* decl = doc.NewDeclaration("xml version=\"1.0\" encoding=\"UTF-8\"");
+        doc.InsertFirstChild(decl);
+        tinyxml2::XMLElement* newRoot = doc.NewElement("Root");
+        doc.InsertEndChild(newRoot);
+        eResult = doc.SaveFile(SAVEFILE.c_str());
+        if (eResult != tinyxml2::XML_SUCCESS){
+            error("Failed to create " + SAVEFILE);
+        }
+        std::cout << SAVEFILE << " created successfully.\n";
     }
 
+    // get root element
+    tinyxml2::XMLElement* root = doc.RootElement();
+    if (!root){
+        error("No root element found in the XML file.");
+    }
+
+    return root;
 }
 
 
 
-bool saveFile(nlohmann::json SAVEFILE)
+bool saveFile(const std::string& SAVEFILE, tinyxml2::XMLDocument& doc)
 {
+    tinyxml2::XMLError eResult = doc.SaveFile(SAVEFILE.c_str());
+    if (eResult != tinyxml2::XML_SUCCESS){
+        error("Could not save XML file.");
+    }
+    std::cout << "File saved successfully.\n";
+    return true;
+}
 
+
+
+void error(const std::string& FAILMESSAGE)
+{
+    std::string newErrorMessage = "ERROR: " + FAILMESSAGE;
+    std::cerr << newErrorMessage;
+    throw std::runtime_error(newErrorMessage);
 }
